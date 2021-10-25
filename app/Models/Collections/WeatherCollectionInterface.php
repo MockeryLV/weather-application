c01718530221fc871493b873588e415631cd47cf
Alexey Chikuns <alexey.chikuns@gmail.com>
2021-10-25 23:19:27 +0300

    uploaded project files

---
+++ app/Models/Collections/WeatherCollectionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Models\Collections;
+
+interface WeatherCollectionInterface{
+
+    public function getForecast(): array;
+    public function toArray(): array;
+}
---
+++ app/Repositories/MidWeatherRepository/MidWeatherRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace App\Repositories\MidWeatherRepository;
+
+use App\Models\Collections\MidWeatherStationCollection;
+use App\Models\MidWeatherStation;
+use App\Repositories\CSVWeatherRepository\CSVWeatherRepository;
+use App\Repositories\JSONWeatherRepository\JSONWeatherRepository;
+use App\Repositories\WeatherRepositoryInterface;
+
+class MidWeatherRepository implements WeatherRepositoryInterface {
+
+    private JSONWeatherRepository $jsonWeatherRepo;
+    private CSVWeatherRepository $csvWeatherRepo;
+
+    private MidWeatherStationCollection $weathers;
+
+    private array $location;
+
+    public function __construct()
+    {
+        $this->csvWeatherRepo = new CSVWeatherRepository();
+        $this->jsonWeatherRepo = new JSONWeatherRepository();
+
+        $stationA = $this->csvWeatherRepo->getAll()->toArray();
+        $stationB = $this->jsonWeatherRepo->getAll()->toArray(true);
+        $weathers = [];
+
+        $this->location =  $this->jsonWeatherRepo->getLocation();
+        for($i = 0; $i < count($stationA); $i++){
+
+
+            $weathers[] = new MidWeatherStation(
+                $stationB[$i]['time'],
+                ($stationB[$i]['temp_f'] + $stationA[$i]['temp_c']) /2,
+                ($stationA[$i]['humidity'] + $stationA[$i]['humidity'] ) / 2,
+                ($stationA[$i]['rain'] + $stationB[$i]['rain']) / 2,
+                $stationA[$i]['light'],
+                $stationB[$i]['battery_lvl'],
+                ($stationA[$i]['wind'] + $stationB[$i]['wind']) / 2
+
+            );
+
+        }
+
+        $this->weathers = new MidWeatherStationCollection($weathers);
+
+
+    }
+
+    public function getAll(): MidWeatherStationCollection{
+
+        return $this->weathers;
+
+
+    }
+
+    public function getByIndex(int $index): ?array{
+
+        if(array_key_exists($index, $this->weathers->toArray())){
+            return $this->weathers->toArray()[$index];
+        }
+
+        return NULL;
+
+
+
+    }
+
+
+    public function getLocation(): array
+    {
+        return $this->location;
+    }
+
+
+}
